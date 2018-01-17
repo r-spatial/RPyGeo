fae0b925057180189c99c2c6058621a79eee0212
fapola <f.polakowski@gmx.de>
2018-01-17 13:49:54 +0100

    formating and test for overwrite and extensions

--- test_geoprocessor.R
+++ test_geoprocessor.R
@@ -1,14 +1,18 @@
 a <- rpygeo_build_env()
 
-a$CheckOutExtension("3D")
+# a$CheckOutExtension("3D")
 py_run_string("arcpy.env.overwriteOutput = True")
 
-rpygeo_geoprocessor(a, "Slope_3d", c("C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.tif",
-                                     "C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw_r.tif"))
+rpygeo_geoprocessor(lib = a, fun = "Slope_3d", args = c("C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.tif",
+                                     "C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw_r.tif"), overwrite = FALSE)
 
 
-rpygeo_geoprocessor(a, "RasterToASCII_conversion", c("C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.tif",
-                                                     "C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.asc"))
+# rpygeo_geoprocessor(a, "RasterToASCII_conversion", c("C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.tif",
+#                                                      "C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.asc"))
+#
 
 
+
+
+# arc gis pro tests
 # exp <- rpygeo_build_env("C:/Program Files/ArcGIS/Pro/bin/Python/Scripts/conda.exe")
