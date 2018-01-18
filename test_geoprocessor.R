7b55923180068e3cbe6c307f70ba918b4344fbe3
fapola <f.polakowski@gmx.de>
2018-01-18 17:53:09 +0100

    minor changes

--- test_geoprocessor.R
+++ test_geoprocessor.R
@@ -1,10 +1,12 @@
 a <- rpygeo_build_env()
 
 # a$CheckOutExtension("3D")
-py_run_string("arcpy.env.overwriteOutput = True")
+# py_run_string("arcpy.env.overwriteOutput = True")
 
-rpygeo_geoprocessor(lib = a, fun = "Slope_3d", args = c("C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.tif",
-                                     "C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw_r.tif"), overwrite = FALSE)
+rpygeo_geoprocessor(lib = a, fun = "Slope_3d", args = c(
+  "C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.tif",
+  "C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw_r.tif"
+), overwrite = TRUE, extensions = "Spatial")
 
 
 # rpygeo_geoprocessor(a, "RasterToASCII_conversion", c("C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.tif",
