3ac72f40a3269355c8eca03a8e8f651c248fa4a5
fapola <f.polakowski@gmx.de>
2018-02-05 15:33:22 +0100

    minor test changes

--- test_geoprocessor.R
+++ test_geoprocessor.R
@@ -3,7 +3,12 @@ a <- rpygeo_build_env()
 rpygeo_geoprocessor(lib = a, fun = "Slope_3d", args = c(
   "C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.tif",
   "C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw_r.tif"
-), overwrite = TRUE, extensions = "Spatial", detect_require_extension = TRUE)
+), overwrite = TRUE, extensions = c("Spatial") , detect_require_extension = TRUE)
+
+
+
+
+
 
 
 # rpygeo_geoprocessor(a, "RasterToASCII_conversion", c("C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.tif",
@@ -17,7 +22,7 @@ rpygeo_geoprocessor(lib = a, fun = "Slope_3d", args = c(
 # exp <- rpygeo_build_env("C:/Program Files/ArcGIS/Pro/bin/Python/Scripts/conda.exe")
 
 # test rpygeo_build_env extended functionallity
-b <- rpygeo_build_env(overwrite = TRUE, extension = "3D")
+b <- rpygeo_build_env(overwrite = TRUE, extensions = c("3d", "Spatial", "na"))
 
 b$Slope_3d("C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw.tif",
            "C:/Users/f/Google Drive/MA/thesis/R_analysis/data/raster_kw_r.tif")
