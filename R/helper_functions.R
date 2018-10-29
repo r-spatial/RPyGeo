7edbacf8c10c215cc94c9224b6736817f94e9565
jannes-m <malnamalja@gmx.de>
2018-10-29 04:13:36 -0700

    remove export from internal helper functions

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -1,23 +1,15 @@
 #' @title  Set overwrite and extension parameters
-#'
 #' @description Internal helper function that sets the overwrite parameter for
 #'  the ArcPy session and can check out multiple extensions.
-#'
 #' @param overwrite If set to `TRUE` (default) existing ArcGIS datasets can be
 #'   overwritten.
-#'
 #' @param extensions Optional character vector listing ArcGIS extension that
 #'   should be enabled. This adds to any extensions that are eventually
 #'   detected by \code{rpygeo_required_extensions}.
-#'
 #' @note This internal function is used by \code{rpygeo_geoprocessor} and
 #'   by \code{rpygeo_build_env}.
-#'
 #' @author Fabian Polakowski and Alexander Brenning
-#'
 #' @keywords internal
-#'
-#' @export
 
 input_check = function (overwrite, extensions) {
 
@@ -45,16 +37,10 @@ input_check = function (overwrite, extensions) {
 }
 
 #' @title  Set workspace
-#'
 #' @description Set workspace for ArcPy environment
-#'
 #' @param path Path to the workspace containing your data
-#'
 #' @author Fabian Polakowski
-#'
 #' @keywords internal
-#'
-#' @export
 
 set_workspace = function (path) {
 
@@ -63,16 +49,10 @@ set_workspace = function (path) {
 }
 
 #' @title  Set scratch workspace
-#'
 #' @description Set scratch workspace for ArcPy environment
-#'
 #' @param path Path to the scratch workspace
-#'
 #' @author Fabian Polakowski and Marc Becker
-#'
 #' @keywords internal
-#'
-#' @export
 
 set_scratch_workspace = function (path) {
 
