c89f21f824195e7260dde414662b9b66f74503e3
Marc Becker <marcbecker@posteo.de>
2018-11-06 20:01:38 +0100

    Add test for ArcGIS Pro

--- tests/testthat/test-rpygeo_built_env.R
+++ tests/testthat/test-rpygeo_built_env.R
@@ -1,15 +1,19 @@
 context("rpygeo_geoprocessor")
+library(RPyGeo)
 library(reticulate)
 
 test_that("rypgeo_build_env's python versions contains arcpy", {
-
   skip_on_cran()
 
   rpygeo_build_env()
 
   testthat::expect_true(py_module_available("arcpy"))
-
 })
 
+test_that("rypgeo_build_env's python versions contains arcpy with ArcGIS Pro", {
+  skip_on_cran()
 
+  rpygeo_build_env(pro = TRUE)
 
+  testthat::expect_true(py_module_available("arcpy"))
+})
